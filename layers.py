8a8a973aaa1dcbb66979afcb90e01e41af3dd560
Administrator <admin@example.com>
2019-10-15 19:23:13 +0800

    save

--- layers.py
+++ layers.py
@@ -136,34 +136,40 @@ def instance_normal(input,name):
 
 def batch_normal(x, is_training=True, name=None, moving_decay=0.9, eps=1e-5,gamma_initializer = tf.constant_initializer(1)):
 
-    # 获取输入维度并判断是否匹配卷积层(4)或者全连接层(2)
-    shape = x.shape
-    assert len(shape) in [2, 4]
-    param_shape = shape[-1]
-    with tf.variable_scope(name,reuse=tf.AUTO_REUSE):
-        # 声明BN中唯一需要学习的两个参数，y=gamma*x+beta
-
-        # 计算当前整个batch的均值与方差
-        axes = list(range(len(shape) - 1))
-        batch_mean, batch_var = tf.nn.moments(x, axes, name='moments')
-
-        # 采用滑动平均更新均值与方差
-        ema = tf.train.ExponentialMovingAverage(moving_decay)
-        def mean_var_with_update():
-            ema_apply_op = ema.apply([batch_mean, batch_var])
-            with tf.control_dependencies([ema_apply_op]):
-                return tf.identity(batch_mean), tf.identity(batch_var)
-
-        # 训练时，更新均值与方差，测试时使用之前最后一次保存的均值与方差
-        mean, var = tf.cond(tf.equal(is_training, True) , mean_var_with_update,
-                            lambda: (ema.average(batch_mean), ema.average(batch_var)))
-
-        gamma = tf.get_variable('gamma', shape = param_shape, initializer = gamma_initializer)
-        beta = tf.get_variable('beta', shape = param_shape, initializer = tf.constant_initializer(0))
-
-        # 最后执行batch normalization
-        return tf.nn.batch_normalization(x, mean, var, beta, gamma, eps)
-
+    with tf.variable_scope(name,reuse = tf.AUTO_REUSE):
+        return tf.contrib.layers.batch_norm(x,
+                                            decay=moving_decay,
+                                            scale=True,
+                                            updates_collections=None,
+                                            is_training=is_training)
+    # # 获取输入维度并判断是否匹配卷积层(4)或者全连接层(2)
+    # shape = x.shape
+    # assert len(shape) in [2, 4]
+    # param_shape = shape[-1]
+    # with tf.variable_scope(name,reuse=tf.AUTO_REUSE):
+    #     # 声明BN中唯一需要学习的两个参数，y=gamma*x+beta
+    #
+    #     # 计算当前整个batch的均值与方差
+    #     axes = list(range(len(shape) - 1))
+    #     batch_mean, batch_var = tf.nn.moments(x, axes, name='moments')
+    #
+    #     # 采用滑动平均更新均值与方差
+    #     ema = tf.train.ExponentialMovingAverage(moving_decay)
+    #     def mean_var_with_update():
+    #         ema_apply_op = ema.apply([batch_mean, batch_var])
+    #         with tf.control_dependencies([ema_apply_op]):
+    #             return tf.identity(batch_mean), tf.identity(batch_var)
+    #
+    #     # 训练时，更新均值与方差，测试时使用之前最后一次保存的均值与方差
+    #     mean, var = tf.cond(tf.equal(is_training, True) , mean_var_with_update,
+    #                         lambda: (ema.average(batch_mean), ema.average(batch_var)))
+    #
+    #     gamma = tf.get_variable('gamma', shape = param_shape, initializer = gamma_initializer)
+    #     beta = tf.get_variable('beta', shape = param_shape, initializer = tf.constant_initializer(0))
+    #
+    #     # 最后执行batch normalization
+    #     return tf.nn.batch_normalization(x, mean, var, beta, gamma, eps)
+    #
 
 
 
