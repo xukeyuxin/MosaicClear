4048ce175ac1ea01ac71e0645d9a4e15acb2a8e2
Administrator <admin@example.com>
2019-10-15 19:31:21 +0800

    save

--- layers.py
+++ layers.py
@@ -103,38 +103,35 @@ def instance_normal(input,name):
 
         return _scale * normalized + _offset
 
-# def batch_normal(input,name = None,is_training=True, moving_decay=0.99):
-#     input_shape = get_shape(input)
-#
-#     gamma_initializer = tf.random_normal_initializer(mean = 0, stddev = 0.02)
-#     beta_initializer = tf.constant_initializer(0.)
-#     with tf.variable_scope(name,reuse=tf.AUTO_REUSE):
-#         gamma = tf.get_variable('gamma',input_shape[-1],initializer = gamma_initializer)
-#         beta = tf.get_variable('beta',input_shape[-1],initializer = beta_initializer)
-#
-#         mean,variance = tf.nn.moments(input, axes = [0,1,2], keep_dims = True)
-#
-#         ema = tf.train.ExponentialMovingAverage(moving_decay)
-#         ema_apply_op = ema.apply([mean, variance])
-#
-#         def mean_vars_update():
-#
-#             with tf.control_dependencies([ema_apply_op]):
-#                 return tf.identity(mean),tf.identity(variance)
-#         if(is_training):
-#             mean, variance = mean_vars_update()
-#         else:
-#             mean, variance = ema.average(mean), ema.average(variance)
-#
-#
-#         epsilon = 1e-5
-#         # inv = (variance + epsilon) ** -0.5
-#         # normalized = (input - mean) * inv
-#     # return gamma * normalized + beta
-#     return tf.nn.batch_normalization(input, mean, variance, beta, gamma, epsilon)
+def _batch_normal(input,name = None,is_training=True, moving_decay=0.99):
+    input_shape = get_shape(input)
+
+    gamma_initializer = tf.random_normal_initializer(mean = 0, stddev = 0.02)
+    beta_initializer = tf.constant_initializer(0.)
+    with tf.variable_scope(name,reuse=tf.AUTO_REUSE):
+        gamma = tf.get_variable('gamma',input_shape[-1],initializer = gamma_initializer)
+        beta = tf.get_variable('beta',input_shape[-1],initializer = beta_initializer)
+
+        mean,variance = tf.nn.moments(input, axes = [0,1,2], keep_dims = True)
+
+        ema = tf.train.ExponentialMovingAverage(moving_decay)
+        ema_apply_op = ema.apply([mean, variance])
+
+        def mean_vars_update():
+
+            with tf.control_dependencies([ema_apply_op]):
+                return tf.identity(mean),tf.identity(variance)
+
+        mean, variance = tf.cond(tf.equal(is_training, True) , mean_var_with_update,
+                            lambda: (ema.average(mean), ema.average(variance)))
+
+        epsilon = 1e-5
+        inv = (variance + epsilon) ** -0.5
+        normalized = (input - mean) * inv
+    return gamma * normalized + beta
 
 
-def batch_normal(x, is_training=True, name=None, moving_decay=0.9, eps=1e-5,gamma_initializer = tf.constant_initializer(1)):
+def batch_normal(x, is_training=True, name=None, moving_decay=0.9):
 
     with tf.variable_scope(name,reuse = tf.AUTO_REUSE):
         return tf.contrib.layers.batch_norm(x,
@@ -142,34 +139,6 @@ def batch_normal(x, is_training=True, name=None, moving_decay=0.9, eps=1e-5,gamm
                                             scale=True,
                                             updates_collections=None,
                                             is_training=is_training)
-    # # 获取输入维度并判断是否匹配卷积层(4)或者全连接层(2)
-    # shape = x.shape
-    # assert len(shape) in [2, 4]
-    # param_shape = shape[-1]
-    # with tf.variable_scope(name,reuse=tf.AUTO_REUSE):
-    #     # 声明BN中唯一需要学习的两个参数，y=gamma*x+beta
-    #
-    #     # 计算当前整个batch的均值与方差
-    #     axes = list(range(len(shape) - 1))
-    #     batch_mean, batch_var = tf.nn.moments(x, axes, name='moments')
-    #
-    #     # 采用滑动平均更新均值与方差
-    #     ema = tf.train.ExponentialMovingAverage(moving_decay)
-    #     def mean_var_with_update():
-    #         ema_apply_op = ema.apply([batch_mean, batch_var])
-    #         with tf.control_dependencies([ema_apply_op]):
-    #             return tf.identity(batch_mean), tf.identity(batch_var)
-    #
-    #     # 训练时，更新均值与方差，测试时使用之前最后一次保存的均值与方差
-    #     mean, var = tf.cond(tf.equal(is_training, True) , mean_var_with_update,
-    #                         lambda: (ema.average(batch_mean), ema.average(batch_var)))
-    #
-    #     gamma = tf.get_variable('gamma', shape = param_shape, initializer = gamma_initializer)
-    #     beta = tf.get_variable('beta', shape = param_shape, initializer = tf.constant_initializer(0))
-    #
-    #     # 最后执行batch normalization
-    #     return tf.nn.batch_normalization(x, mean, var, beta, gamma, eps)
-    #
 
 
 
